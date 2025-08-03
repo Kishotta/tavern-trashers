c9e780199363be79b687e4ece2d375244a5f8d8c
Connor Eaves <kishotta@gmail.com>
2025-08-02 23:26:40 -0500

    Moves common testing project
    Moves the common testing project to a domain tests project and updates the solution file.
    
    Also adds a new project for campaign domain tests that exercises domain event dispatching, and updates common domain test helpers to use Shouldly.

--- src/api/common/TavernTrashers.Api.Common.Domain.Tests/TestBase.cs
+++ src/api/common/TavernTrashers.Api.Common.Domain.Tests/TestBase.cs
@@ -1,19 +1,19 @@
-using Bogus;
+﻿using Bogus;
+using Shouldly;
 using TavernTrashers.Api.Common.Domain.Entities;
 
-namespace TavernTrashers.Api.Common.Testing;
+namespace TavernTrashers.Api.Common.Domain.Tests;
 
 public abstract class TestBase
 {
 	protected static readonly Faker Faker = new();
-	
+
 	public static TDomainEvent AssertDomainEventWasPublished<TDomainEvent>(EntityBase entity)
-		where TDomainEvent : IDomainEvent
+		where TDomainEvent : class, IDomainEvent
 	{
 		var domainEvent = entity.GetDomainEvents().OfType<TDomainEvent>().SingleOrDefault();
-        
-		if(domainEvent is null)
-			throw new Exception($"{typeof(TDomainEvent).Name} was not published");
+
+		domainEvent.ShouldNotBeNull($"{typeof(TDomainEvent).Name} was not published");
 
 		return domainEvent;
 	}
